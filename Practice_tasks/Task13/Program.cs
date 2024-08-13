2533950f6b58058e05e7fb58989dffd38577af5f
Andrijrbsh <aruabush@gmail.com>
2024-08-13 23:02:37 +0300

    new file:   Practice_tasks/Task13/Task13.csproj

---
+++ Practice_tasks/Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿System.Console.WriteLine("Enter a number: ");
+        string? inputFromUser = Console.ReadLine();
+        if (int.TryParse(inputFromUser, out int outputFromUser))
+        {
+            PrintRepeated(outputFromUser, 3);
+            System.Console.WriteLine();
+
+            PrintWithSpace(outputFromUser);
+
+            PrintRepeated(outputFromUser, 3);
+            System.Console.WriteLine();
+
+            System.Console.ReadKey();
+        }
+
+        static void PrintRepeated(int number, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                System.Console.Write("{0}", number);
+            }
+        }
+        static void PrintWithSpace(int number)
+        {
+            System.Console.WriteLine("{0} {0}", number);
+        }
