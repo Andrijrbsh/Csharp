230bfa3493848118755093bce558623dd4750916
Andrijrbsh <aruabush@gmail.com>
2024-08-14 17:41:18 +0300

    new file:   PracticeCSharpAddition/TaskMathArrayUseRecursion/TaskMathArrayUseRecursion.csproj

---
+++ PracticeCSharpAddition/TaskMathArrayUseRecursion/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numbers = {3,5,6};
+int sum = SumAllNumFromArray(numbers, numbers.Length - 1);
+Console.WriteLine("Sum is {0}", sum);
+static int SumAllNumFromArray(int[] numbers, int index)
+{
+    if (index < 0)
+    {
+        return 0;
+    }
+    return numbers[index] + SumAllNumFromArray(numbers, index - 1);
+}
