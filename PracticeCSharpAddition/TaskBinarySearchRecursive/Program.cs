f7ce1b1e3158fb1dc0a4bc2b3deaee3e2b112b92
Andrijrbsh <aruabush@gmail.com>
2024-08-15 16:57:22 +0300

    modified:   C-tutorial.generated.sln

---
+++ PracticeCSharpAddition/TaskBinarySearchRecursive/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] numbers = new int[] {1,2,3,4,5};
+System.Console.WriteLine("Please input number from 1 to 5, what you need to pick from array");
+int rightNumber = int.Parse(Console.ReadLine());
+
+int index = BinarySearch(numbers, 0, numbers.Length - 1, rightNumber);
+
+ if (index != -1)
+        {
+            Console.WriteLine($"Number {rightNumber} found at index {index}.");
+        }
+        else
+        {
+            Console.WriteLine($"Number {rightNumber} not found in the array.");
+        }
+
+static int BinarySearch(int[] numbers, int left, int right, int rightNumber)
+{
+    if (left<=right)
+    {
+       int middle = (left + right)/2;
+       if (numbers[middle] == rightNumber)
+       {
+            return middle;
+       }
+       else if (numbers[middle] > rightNumber)
+       {
+            return BinarySearch(numbers, left, middle - 1, rightNumber);
+       }
+       else
+       {
+            return BinarySearch(numbers, middle + 1, right, rightNumber);
+       }
+    }
+    return -1;
+}
