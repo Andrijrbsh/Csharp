0dcfac706b266378786ee0a8be7e68fd4830efd1
Andrijrbsh <aruabush@gmail.com>
2024-08-15 14:49:04 +0300

    new file:   PracticeCSharpAddition/TaskSumFromList/TaskSumFromList.csproj

---
+++ PracticeCSharpAddition/TaskSumFromList/Program.cs
@@ -0,0 +1,12 @@
+﻿List<int> numbers = new List<int>(){5,4,9};
+int sum = SumFromList(numbers, numbers.Count - 1);
+System.Console.WriteLine("Sum is {0}", sum);
+
+static int SumFromList(List<int> numbers, int index)
+{
+    if (index < 0)
+    {
+        return 0;
+    }
+    return numbers[index] + SumFromList(numbers, index - 1);
+}
