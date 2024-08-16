c8255c5d15b0ac7be9b32dff121dfbc501a121c7
Andrijrbsh <aruabush@gmail.com>
2024-08-16 20:14:07 +0300

    new file:   softserve/C#/homework/Topic4/4.1/Program.cs

---
+++ softserve/C#/homework/Topic4/4.1/Program.cs
@@ -0,0 +1,28 @@
+﻿public static class Car
+{
+    public string Name { get; set; };
+    private string color;
+
+    public string Color 
+    { 
+        get { return color} ; 
+        set { color = value} ; 
+    };
+    public double Price { get; set; };
+    public readonly string CompanyName;
+    public Car()   
+    { 
+        Name = "Ford Mustang"; 
+        Color = "black"; 
+        Price = 18000; 
+        CompanyName = "Ford";
+    }
+
+    public Car(string name, string color, double price, string companyName)
+    {
+        Name = name;
+        Color = color;
+        Price = price;
+        CompanyName = companyName; 
+    }
+}
